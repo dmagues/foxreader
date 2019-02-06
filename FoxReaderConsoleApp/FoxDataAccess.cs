8832b1d57fbadee104d4740fed40070382cde078
Daniel Magues <dmagues@campoecu.com>
2019-02-06 09:35:35 -0500

    init version foxreader

---
+++ FoxReaderConsoleApp/FoxDataAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoxReaderConsoleApp
+{
+    class FoxDataAccess
+    {
+        public DataSet GetData(string dbfile)
+        {
+            using (var conn = new OleDbConnection()) {
+                //conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.12.0;Data Source=c:\foxfiles;Extended Properties=dBASE IV;User ID=Admin;Password=;";
+                conn.ConnectionString = @"Provider=VFPOLEDB.1;Data Source=E:\BODIMP09;";
+                var adapter = new OleDbDataAdapter($"select * from {dbfile}", conn);
+                var ds = new DataSet();
+                adapter.Fill(ds);
+                return ds;
+            }           
+
+        }
+
+    }
+}
---
+++ FoxReaderConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoxReaderConsoleApp.FoxDSTableAdapters;
+using FoxReaderConsoleApp.SqlServerDSTableAdapters;
+
+namespace FoxReaderConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var foxDA = new FoxDataAccess();
+            var ta = new ItemTableAdapter();
+            var sqlta = new ITEMTableAdapter();
+            var sqlDt = new SqlServerDS.ITEMDataTable();
+            var foxDt = ta.GetDataItrm();
+
+            foreach(FoxDS.ItemRow r in foxDt)
+            {
+                Console.WriteLine(string.Join(",",r.ItemArray));
+                sqlDt.AddITEMRow(r.cod_item, r.cod_iten, r.nomb_item, r.cod_par, r.precio_pro, r.existe, r.precio_ult,
+                    r.unidad, r.uti_item1, r.uti_item2, r.uti_item3, r.maximo, r.minimo, r.factor, r.prsocio, r.prventa,
+                    r.protros, r.cta_mer, r.cta_vta, r.grupo, r.uso);
+
+            }
+
+            sqlta.Update(sqlDt);
+
+            Console.ReadLine();
+        }
+    }
+}
